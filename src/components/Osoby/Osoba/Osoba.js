29d0376d50d4eb4b1e6019bcd6d8ef6d7e8758df
777z <7b.onus583@gmail.com>
2018-10-09 20:55:08 +0200

    statelessMod2

--- src/components/Osoby/Osoba/Osoba.js
+++ src/components/Osoby/Osoba/Osoba.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import zKlas from './Osoba.css'
 
 
-const osoba = (props) => {
-    return (
-    <div className={zKlas.Osoba}>
-        <h4>Moje meno je {props.meno}.</h4>
-        <p onClick={props.clique}>Pochadzam z {props.lokacia}. A mam {Math.floor(Math.random()*100)} rokov lmao.</p>
-        <h1>{props.children}</h1>
-        <input type='text' onChange={props.zmenena} value={props.meno} />
+class Osoba extends Component {
+    render(){
+    return <div className={zKlas.Osoba}>
+    <h4>Moje meno je {this.props.meno}.</h4>
+    <p onClick={this.props.clique}>Pochadzam z {this.props.lokacia}. A mam {Math.floor(Math.random()*100)} rokov lmao.</p>
+    <h1>{this.props.children}</h1>
+    <input type='text' onChange={this.props.zmenena} value={this.props.meno} />
     </div>
-    )
+    }
 }
 
-export default osoba
 
-// xaras
-// x1 --
+export default Osoba
+
