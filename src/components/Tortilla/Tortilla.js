6ecaa9a12f2d11ebcf4ff4678a974f2806a575f7
777z <7b.onus583@gmail.com>
2018-10-21 16:42:05 +0200

    TRewrittenMod

--- src/components/Tortilla/Tortilla.js
+++ src/components/Tortilla/Tortilla.js
@@ -4,18 +4,18 @@ import classes from './Tortilla.css';
 import TortillaIngredient from './TortillaIngredient/TortillaIngredient';
 //commitTest3
 const tortilla = ( props ) => {
-    let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <TortillaIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
-    }
+    let transformedIngredients = Object.keys( props.ingredients)
+           .map(ingKey => {
+               return [...Array(props.ingredients[ingKey])].map((_,i) => {
+                   return <TortillaIngredient key={ingKey + i} type={ingKey} />
+               })
+           } )
+           .reduce((arr, el) => {
+               return arr.concat(el)
+           }, [])
+        if (transformedIngredients.length === 0){
+            transformedIngredients = <p>Please, start adding ingredients</p>
+        }
     return (
         <div className={classes.Tortilla}>
             <TortillaIngredient type="bread-top" />
