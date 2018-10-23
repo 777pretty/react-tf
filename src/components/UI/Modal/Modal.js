13151093dd543d8fadc57e05148816ede2ac0cae
777z <7b.onus583@gmail.com>
2018-10-23 20:45:16 +0200

    backdropMod

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
 import classes from './Modal.css'
+import Aukz from '../../../hoc/Aukz'
+import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => {
-   return <div className={classes.Modal} 
+const modal = (props) => (
+   
+   <Aukz>
+   <Backdrop disp={props.disp} cliked={props.modalOut}/>
+   <div className={classes.Modal} 
                style={{transform: props.disp ? 'translateY(0)' : 'translateY(-100vh)',
                        opacity: props.disp ?'1':'0'}}>
-                {props.children}
-          </div>
-}
+                {props.children}    
+   </div>
+   </Aukz>
+)
 
 export default modal
