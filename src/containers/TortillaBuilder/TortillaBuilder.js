cadce44cad76dd8f92d2e6891255ee82dfbf584f
777z <7b.onus583@gmail.com>
2018-10-28 15:33:31 +0100

    checkButtonMod9

--- src/containers/TortillaBuilder/TortillaBuilder.js
+++ src/containers/TortillaBuilder/TortillaBuilder.js
@@ -108,7 +108,7 @@ class TortillaBuilder extends Component {
                 <Modal disp={this.state.reviewing} modalOut={this.reviewOutHandler}>
                     <OrderReview ingredients={this.state.ingredients}
                                  reviewCanceled={this.reviewOutHandler}
-                                 reviewContinue={this.reviewContinueHandler} />    
+                                 reviewContinued={this.reviewContinueHandler} />    
                 </Modal>
                 <Tortilla ingredients={this.state.ingredients} />
                 <BuildControls 
