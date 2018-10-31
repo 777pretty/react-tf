26a2091cf158213191f652a6538e983aeaef83f9
777z <7b.onus583@gmail.com>
2018-10-31 15:36:31 +0100

    toolbarUpd

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Aukz from '../../hoc/Aukz';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = ( props ) => (
     <Aukz>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
