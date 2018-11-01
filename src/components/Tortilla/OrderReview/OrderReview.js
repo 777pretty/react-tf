a7e5559ca55e923703aca79f7eda9dc3efaa59ee
777z <7b.onus583@gmail.com>
2018-11-01 14:12:27 +0100

    improvingPerfWLifecycleUpd

--- src/components/Tortilla/OrderReview/OrderReview.js
+++ src/components/Tortilla/OrderReview/OrderReview.js
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aukz from '../../../hoc/Aukz'
 import Button from '../../UI/Button/Button'
 
-const orderReview = (props) => {
-    const ingredientsReview = Object.keys(props.ingredients)
+class OrderReview extends Component {
+    // this doesnt have to be a stateful component anymore
+    componentWillUpdate(){
+        console.log('[OrderReview] WillUpdate')
+    }
+
+    render(){
+
+        const ingredientsReview = Object.keys(this.props.ingredients)
         .map(ingKey => {
-            return <li key={ingKey}> <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]} </li>
+            return <li key={ingKey}> <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]} </li>
         })
 
-    return <Aukz>
+
+        return(
+            <Aukz>
              <h2>Review</h2>
              <p>For your tasty Tortilla™ you picked the following ingredients:</p>
              <ul>
                 {ingredientsReview}
              </ul>
-             <p><strong>Total cost: {props.totalCost}€</strong></p>
+             <p><strong>Total cost: {this.props.totalCost}€</strong></p>
              <p>Proceed to checkout?</p>
-            <Button buttonType="Danger" clickd={props.reviewCanceled}>Cancel</Button>
-            <Button buttonType="Success" clickd={props.reviewContinue}>Continue</Button>
+            <Button buttonType="Danger" clickd={this.props.reviewCanceled}>Cancel</Button>
+            <Button buttonType="Success" clickd={this.props.reviewContinue}>Continue</Button>
            </Aukz>
 
+        )
+    }
 }
 
-export default orderReview
+export default OrderReview
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Modal.css'
 import Aukz from '../../../hoc/Aukz'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-   
-   <Aukz>
-   <Backdrop disp={props.disp} cliked={props.modalOut}/>
-   <div className={classes.Modal} 
-               style={{transform: props.disp ? 'translateY(0)' : 'translateY(-100vh)',
-                       opacity: props.disp ?'1':'0'}}>
-                {props.children}    
-   </div>
-   </Aukz>
-)
+class Modal extends Component {
 
-export default modal
+        shouldComponentUpdate(nextProps, nextState){
+                return nextProps.disp !== this.props.disp
+        }
+
+        componentWillUpdate(){
+                return console.log('[Modal] WillUpdate')
+        }
+
+        render(){
+
+
+        return(
+        <Aukz>
+        <Backdrop disp={this.props.disp} cliked={this.props.modalOut}/>
+        <div className={classes.Modal} 
+                style={{transform: this.props.disp ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.disp ?'1':'0'}}>
+                        {this.props.children}    
+        </div>
+        </Aukz>
+        )
+   }
+}
+
+export default Modal
