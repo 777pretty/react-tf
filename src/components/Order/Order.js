52949e2d21ef42c04537bf367b9e2db6f7b42c00
777z <7b.onus583@gmail.com>
2018-11-23 12:01:36 +0100

    Fix Order value not showing properly

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,7 @@ import classes from './Order.css'
 const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients: Salad (1) </p>
-        <p>Cost: <strong>{props.cost} €</strong></p>
+        <p>Cost: <strong>{Number.parseFloat(props.cost).toFixed} €</strong></p>
     </div>
 )
 
