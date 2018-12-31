b514e8d9c5022f87433ff3e7cc22aad5f70436cf
777z <7b.onus583@gmail.com>
2018-12-31 22:42:04 +0100

    Start adding order actions

---
+++ src/store/actions/order.js
@@ -0,0 +1,30 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseTortillaSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_TORTILLA_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseTortillaFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_TORTILLA_FAIL,
+        error: error
+    };
+};
+
+export const purchaseTortillaStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+        .then(response => {
+            console.log(response.data)
+            dispatch(purchaseTortillaSuccess(response.data, orderData))
+        })
+        .catch(error => {
+            dispatch(purchaseTortillaFail(error))
+        });
+    };
+};
