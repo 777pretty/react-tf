46dda8ed031aa1350202daf63f317e45b6b485f3
777z <7b.onus583@gmail.com>
2019-01-02 18:17:17 +0100

    Polish order reducer switch statements

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,29 +7,44 @@ const initialState = {
     purchased: false
 };
 
+const purchaseInit = (state, action) => {
+    return updObj(state, {purchased: false});
+};
+const purchaseTortillaBegin = (state, action) => {
+    return updObj(state, {loading: true});
+};
+const purchaseTortillaSuccess = (state, action) => {
+    const newWOrder = {
+        ...action.orderData,
+        id: action.orderId
+    };
+    return updObj(state, {loading: false,
+                          purchased: true,
+                          orders: state.orders.concat(newWOrder)});
+}
+const purchaseTortillaFailed = (state, action) => {
+    return updObj(state, {loading: false})
+}
+const fetchOrdBegin = (state, action) => {
+    return updObj(state, {loading: true})
+}
+const fetchOrdSuccess = (state, action) => {
+    return updObj(state, {orders: action.orders,
+        loading: false});
+}
+const fetchOrdFailed = (state, action) => {
+    return updObj(state, {loading: false});
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return updObj(state, {purchased: false});
-        case actionTypes.PURCHASE_TORTILLA_BEGIN:
-            return updObj(state, {loading: true});
-        case actionTypes.PURCHASE_TORTILLA_SUCCESS:
-            const newWOrder = {
-                ...action.orderData,
-                id: action.orderId
-            };
-            return updObj(state, {loading: false,
-                                  purchased: true,
-                                  orders: state.orders.concat(newWOrder)});
-        case actionTypes.PURCHASE_TORTILLA_FAILED:
-            return updObj(state, {loading: false})
-        case actionTypes.FETCH_ORD_BEGIN:
-            return updObj(state, {loading: true})
-        case actionTypes.FETCH_ORD_SUCCESS:
-            return updObj(state, {orders: action.orders,
-                                  loading: false});
-        case actionTypes.FETCH_ORD_FAILED:
-            return updObj(state, {loading: false});
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_TORTILLA_BEGIN: return purchaseTortillaBegin(state, action);
+        case actionTypes.PURCHASE_TORTILLA_SUCCESS: return purchaseTortillaSuccess(state, action);
+        case actionTypes.PURCHASE_TORTILLA_FAILED: return purchaseTortillaFailed(state, action);
+        case actionTypes.FETCH_ORD_BEGIN: return fetchOrdBegin(state, action);
+        case actionTypes.FETCH_ORD_SUCCESS: return fetchOrdSuccess(state, action);
+        case actionTypes.FETCH_ORD_FAILED: return fetchOrdFailed(state, action);
         default:
             return state;
     }
