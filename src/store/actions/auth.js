032a114160b8a755a412601de1ad83931b56e588
777z <7b.onus583@gmail.com>
2019-01-04 23:57:18 +0100

    Fixed routing connection bugs

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -36,7 +36,7 @@ export const checkAuthTimeout = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
             dispatch(logout());
-        }, expirationTime * 500);
+        }, expirationTime * 1000);
     };
 };
 
@@ -56,7 +56,7 @@ export const auth = (email, password, alreadySigned) => {
         axios.post(url, authData)
              .then(res => {
                  console.log(res);
-                 const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 500);
+                 const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
                  localStorage.setItem('token', res.data.idToken);
                  localStorage.setItem('expirationDate', expirationDate);
                  localStorage.setItem('userId', res.data.localId);
@@ -83,12 +83,12 @@ export const authStateCheck = () => {
             dispatch(logout());
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if (expirationDate > new Date()) {
-                dispatch(authSuccess(token));
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
             } else {
-                const userId = localStorage.getItem('userId')
-                dispatch(logout(token, userId));
-                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ));
             };
         };
     };
